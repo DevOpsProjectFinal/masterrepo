661c22357fc1ca89e29adea38d5538fa3ca67992
swapna15 <swapnakm15@gmail.com>
2025-01-03 13:29:14 -0600

    Updated terraform version and aws version required for the terraform modules

--- terraform/eks-cluster/versions.tf
+++ terraform/eks-cluster/versions.tf
@@ -1,9 +1,10 @@
 terraform {
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.8.4"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.52.0"
     }
   }
+  experiments = [module_variable_optional_attrs] # Ensure this is needed for your configuration
 }
