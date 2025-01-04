6c1b2bfec570a3d8ea2bfefd8511380a1a7df77f
swapna15 <swapnakm15@gmail.com>
2025-01-04 12:34:27 -0600

    Updated the kms key alias to be unique

--- terraform/eks-cluster/main.tf
+++ terraform/eks-cluster/main.tf
@@ -39,23 +39,12 @@ resource "aws_kms_key" "eks" {
   enable_key_rotation     = true
 }
 
-resource "aws_eks_cluster" "main" {
-  name     = module.eks.cluster_name
-  role_arn = module.eks.cluster_iam_role_arn
-
-  vpc_config {
-    subnet_ids = module.vpc.private_subnets
-  }
-
-  encryption_config {
-    provider {
-      key_arn = aws_kms_key.eks.arn
-    }
-    resources = ["secrets"]
-  }
+resource "aws_kms_alias" "eks" {
+  name          = "alias/${var.cluster_name}-eks-${timestamp()}"
+  target_key_id = aws_kms_key.eks.key_id
 }
 
-resource "aws_kms_alias" "eks" {
-  name          = "alias/${var.cluster_name}-eks"
-  target_key_id = aws_kms_key.eks.key_id
+resource "aws_cloudwatch_log_group" "eks" {
+  name = "/aws/eks/${var.cluster_name}/cluster-${timestamp()}"
+  retention_in_days = 90
 }
