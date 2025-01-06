355f582537fdd7f002772799f87c29d190442c47
swapna15 <swapnakm15@gmail.com>
2025-01-05 18:36:06 -0600

    Updated with auto eks creation

--- terraform/eks-cluster/main.tf
+++ terraform/eks-cluster/main.tf
@@ -1,92 +1,86 @@
-  provider "aws" {
-    region = var.aws_region
-  }
+provider "aws" {
+  region = local.region
+}
 
-  resource "random_string" "suffix" {
-    length  = 8
-    special = false
-  }
-
-  module "vpc" {
-    source  = "terraform-aws-modules/vpc/aws"
-    version = "~> 5.0"
-    name    = "eks-vpc"
-    cidr    = var.vpc_cidr
-    enable_dns_hostnames = true
-    enable_dns_support   = true
-    private_subnets      = var.private_subnets
-    public_subnets       = var.public_subnets
-    azs                  = var.availability_zones
-  }
-
-  module "eks" {
-    source  = "terraform-aws-modules/eks/aws"
-    version = "~> 20.0"
-    
-    cluster_name    = var.cluster_name
-    cluster_version = "1.31"
-    vpc_id          = module.vpc.vpc_id
-    subnet_ids      = module.vpc.private_subnets
-
-    self_managed_node_groups = {
-      eks_nodes = {
-        desired_capacity = var.node_group_size
-        max_capacity     = 2
-        min_capacity     = 1
-        instance_type    = var.instance_type
-      }
-    }
-  }
-
-  data "aws_iam_policy_document" "this" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey"
-    ]
-    resources = [
-      "arn:aws:kms:${var.aws_region}:${var.account_id}:key/${var.kms_key_id}"
-    ]
-    principals {
-      type        = "AWS"
-      identifiers = [
-        for principal in var.principals : principal if principal != null
-      ]
-    }
+data "aws_availability_zones" "available" {
+  # Exclude local zones
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
   }
 }
 
-  resource "aws_kms_key" "eks" {
-    description             = "EKS Cluster KMS Key"
-    deletion_window_in_days = 10
-    enable_key_rotation     = true
+locals {
+  name            = "ex-${basename(path.cwd)}"
+  cluster_version = "1.31"
+  region          = "us-west-2"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Test       = local.name
+    GithubRepo = "terraform-aws-eks"
+    GithubOrg  = "terraform-aws-modules"
+  }
+}
+
+################################################################################
+# EKS Module
+################################################################################
+
+module "eks" {
+  source = "../.."
+
+  cluster_name                   = local.name
+  cluster_version                = local.cluster_version
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
   }
 
-  resource "aws_kms_alias" "eks" {
-    name          = "alias/${var.cluster_name}-eks-${random_string.suffix.result}"
-    target_key_id = aws_kms_key.eks.key_id
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  tags = local.tags
+}
+
+module "disabled_eks" {
+  source = "../.."
+
+  create = false
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
   }
 
-  resource "aws_cloudwatch_log_group" "eks" {
-    name              = "/aws/eks/${var.cluster_name}/cluster-${random_string.suffix.result}"
-    retention_in_days = 90
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
   }
 
-  output "vpc_id" {
-    description = "The ID of the VPC"
-    value       = module.vpc.vpc_id
-  }
-
-  output "kms_key_id" {
-    description = "The ID of the KMS key"
-    value       = aws_kms_key.eks.id
-  }
-
-  output "log_group_name" {
-    description = "The name of the CloudWatch log group"
-    value       = aws_cloudwatch_log_group.eks.name
-  }
+  tags = local.tags
+}
