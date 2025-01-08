5034d94c0e9d4a89a52566a4a9046c8966797a52
swapna15 <swapnakm15@gmail.com>
2025-01-08 12:20:37 -0600

    Updates to eks cluster creation with karpenter and fargate profiles

--- terraform/eks-cluster/modules/vpc/main.tf
+++ terraform/eks-cluster/modules/vpc/main.tf
@@ -7,6 +7,9 @@ module "vpc" {
   enable_nat_gateway = true
   enable_dns_hostnames = true
 
+  subnet_ids          = var.subnet_ids
+  security_group_ids  = var.security_group_ids
+
   intra_subnets   = [for k, v in var.availability_zones : cidrsubnet(var.vpc_cidr, 8, k + 52)]
 
   # Optional, if you want to assign a name to the VPC (this is supported in the module)
