54203e1a4519c3e5cedb60c4adea7c37a067a90d
swapna15 <swapnakm15@gmail.com>
2025-01-08 18:20:18 -0600

    Updated the single nat gateway creation

--- terraform/eks-cluster/modules/vpc/main.tf
+++ terraform/eks-cluster/modules/vpc/main.tf
@@ -2,11 +2,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   cidr            = var.vpc_cidr
   azs             = var.availability_zones
-  private_subnets = var.private_subnets
+  private_subnets = [var.private_subnets[0]]
   public_subnets  = var.public_subnets
   enable_nat_gateway = true
   enable_dns_hostnames = true
-
+  
   # Optional, if you want to assign a name to the VPC (this is supported in the module)
   name = var.vpc_name
 }
