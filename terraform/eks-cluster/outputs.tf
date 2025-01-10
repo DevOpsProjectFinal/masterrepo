9ea8d7b72de6f977ecdae84d02101aa28f82b342
swapna15 <swapnakm15@gmail.com>
2025-01-10 14:50:12 -0600

    Updated

--- terraform/eks-cluster/outputs.tf
+++ terraform/eks-cluster/outputs.tf
@@ -2,49 +2,9 @@
 # Cluster
 ################################################################################
 
-output "cluster_arn" {
-  description = "The Amazon Resource Name (ARN) of the cluster"
-  value       = module.eks.cluster_arn
-}
-
 output "cluster_endpoint" {
   description = "Endpoint for your Kubernetes API server"
   value       = "https://DDCF651519C65187EAF462B3FB3EE5B9.gr7.us-east-1.eks.amazonaws.com"
-}
-
-output "cluster_id" {
-  description = "The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts"
-  value       = module.eks.cluster_id
-}
-
-output "vpc_id" {
-  description = "The VPC id"
-  value = module.vpc.vpc_id
-}
-
-output "cluster_platform_version" {
-  description = "Platform version for the cluster"
-  value       = module.eks.cluster_platform_version
-}
-
-output "cluster_status" {
-  description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
-  value       = module.eks.cluster_status
-}
-
-output "cluster_primary_security_group_id" {
-  description = "Cluster security group that was created by Amazon EKS for the cluster. Managed node groups use this security group for control-plane-to-data-plane communication. Referred to as 'Cluster security group' in the EKS console"
-  value       = module.eks.cluster_primary_security_group_id
-}
-
-output "cluster_service_cidr" {
-  description = "The CIDR block where Kubernetes pod and service IP addresses are assigned from"
-  value       = module.eks.cluster_service_cidr
-}
-
-output "cluster_ip_family" {
-  description = "The IP family used by the cluster (e.g. `ipv4` or `ipv6`)"
-  value       = module.eks.cluster_ip_family
 }
 
 output "cluster_name" {
