468cc5ac7922eccd79099da8fe2b9b60fd77323b
swapna15 <swapnakm15@gmail.com>
2025-01-11 12:11:13 -0600

    Updated with the branch detail

--- terraform/eks-cluster/modules/eks/variables.tf
+++ terraform/eks-cluster/modules/eks/variables.tf
@@ -13,41 +13,6 @@ variable "cluster_version" {
   default     = "1.31"
 }
 
-
-variable "fargate_profiles" {
-  description = "Map of Fargate profiles to create"
-  default = {
-    default = {
-      selectors = [
-        {
-          namespace = "default"
-        },
-        {
-          namespace = "kube-system"
-        }
-      ]
-    }
-  }
-}
-
-variable "cluster_security_group_id" {
-  description = "The ID of the security group to use for the EKS cluster"
-  type        = string
-  default     = "sg-devops-project-eks-cluster"
-}
-
-variable "create_node_sg" {
-  description = "Whether to create a new security group for the nodes"
-  type        = bool
-  default     = true
-}
-
-variable "node_security_group_id" {
-  description = "The ID of the security group to use for the nodes"
-  type        = string
-  default     = "ng-devops-project-eks-cluster"
-}
-
 variable "vpc_id" {
   description = "The ID of the VPC where the EKS cluster will be deployed"
   type        = string
