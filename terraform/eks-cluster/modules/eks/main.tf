4b95569b781b6c804c8d1d5d74db17f0fc52d8a2
swapna15 <swapnakm15@gmail.com>
2025-01-13 09:38:15 -0600

    Recreating EKS cluster with node groups for fargate profile can launch the instances

--- terraform/eks-cluster/modules/eks/main.tf
+++ terraform/eks-cluster/modules/eks/main.tf
@@ -8,7 +8,6 @@ module "eks" {
   subnet_ids          = var.subnet_ids
   control_plane_subnet_ids = var.control_plane_subnet_ids
 
-
   # Optional
   cluster_endpoint_public_access = true
 
@@ -17,4 +16,18 @@ module "eks" {
 
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2_ARM_64"
+      instance_types = ["t3.micro"]
+
+      min_size = 2
+      max_size = 3
+      # This value is ignored after the initial creation
+      # https://github.com/bryantbiggs/eks-desired-size-hack
+      desired_size = 1
+    }
+  }
 }
--- terraform/eks-cluster/modules/vpc/main.tf
+++ terraform/eks-cluster/modules/vpc/main.tf
@@ -6,6 +6,7 @@ module "vpc" {
   public_subnets  = var.public_subnets
   enable_nat_gateway = true
   enable_dns_hostnames = true
+  single_nat_gateway = true
 
   # Optional, if you want to assign a name to the VPC (this is supported in the module)
   name = var.vpc_name
