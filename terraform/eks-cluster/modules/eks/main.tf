7310dbdc52da991056667806e67179f9833504a7
swapna15 <swapnakm15@gmail.com>
2025-01-13 10:28:04 -0600

    Recreating EKS cluster with node groups for fargate profile can launch the instances -- added back github action flow for apply comd and min and desired size is matched

--- terraform/eks-cluster/modules/eks/main.tf
+++ terraform/eks-cluster/modules/eks/main.tf
@@ -23,7 +23,7 @@ module "eks" {
       ami_type       = "AL2_ARM_64"
       instance_types = ["t3.micro"]
 
-      min_size = 2
+      min_size = 1
       max_size = 3
       # This value is ignored after the initial creation
       # https://github.com/bryantbiggs/eks-desired-size-hack
