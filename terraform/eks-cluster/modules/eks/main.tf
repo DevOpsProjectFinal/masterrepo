e8f7f0e7b15d6b0a80b1b2c94b5f594807fa6029
swapna15 <swapnakm15@gmail.com>
2025-01-16 12:15:54 -0600

    Updated service account creation and irsa for load balancer controller in eks fixed terraform plan

--- terraform/eks-cluster/modules/eks/main.tf
+++ terraform/eks-cluster/modules/eks/main.tf
@@ -17,17 +17,20 @@ module "eks" {
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
+  # IRSA for AWS Load Balancer Controller
+  enable_irsa = true
+
   #eks_managed_node_groups = {
   #  devops_project_eks_ng = {
   #    # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
   #    ami_type       = "AL2_x86_64"
   #    instance_types = ["t3.micro"]
 
-  #    min_size = 1
+  #    min_size = 3
   #    max_size = 10
   #    # This value is ignored after the initial creation
   #    # https://github.com/bryantbiggs/eks-desired-size-hack
-  #    desired_size = 1
+  #    desired_size = 3
   #    disk_size        = 10  # Specify the root volume size in GB (minimum is 10 GB)
   #  }
   #}
